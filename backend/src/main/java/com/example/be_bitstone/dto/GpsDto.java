8230b3f1566e66574bceb21642bd065b29e8aed6
humeniucDan <humeniuc.dan.stefan@gmail.com>
2025-05-08 18:44:04 +0300

    refactorized filebase service

---
+++ backend/src/main/java/com/example/be_bitstone/dto/GpsDto.java
@@ -0,0 +1,13 @@
+package com.example.be_bitstone.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class GpsDto {
+    private Double lat;
+    private Double lng;
+}
